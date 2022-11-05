92ede3e5b0783578eb6371f8d90e39bb97a1ec93
unknown <anthonytcool@hotmail.com>
2022-11-05 14:33:58 +0700

    create privateRoute and Dashboard Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import TopNav from './components/TopNav'
+import PrivateRoute from './components/PrivateRoute'
+//components
 import Home from './booking/Home'
 import Login from './auth/Login'
 import Register from './auth/Register'
-import TopNav from './components/TopNav'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import Dashboard from './user/Dashboard'
+import DashboardSeller from './user/DashboardSeller'
 
 /**
  * Lets create TopNavigation/menu bar so that we can easily TopNavigate between pages
@@ -20,6 +24,24 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+
+        <Route
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path='/dashboard/seller'
+          element={
+            <PrivateRoute>
+              <DashboardSeller />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/user/DashboardSeller.js
@@ -0,0 +1,18 @@
+import DashboardNav from '../components/DashboardNav'
+const DashboardSeller = () => {
+  return (
+    <>
+      <div className='container-fluid bg-secondary p-5'>
+        <h1>Dashboard</h1>
+      </div>
+      <div className='container-fluid p-4'>
+        <DashboardNav />
+      </div>
+
+      <div className='container'>
+        <p>Show all hotels users have posted and a button to add new</p>
+      </div>
+    </>
+  )
+}
+export default DashboardSeller
