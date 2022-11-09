0ce1d9a384108274b4c021752bb0c0e98e893fe3
unknown <anthonytcool@hotmail.com>
2022-11-09 14:29:56 +0700

    Save Stripe Account Id to DB

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,4 +1,20 @@
+import User from '../models/user'
+import Stripe from 'stripe'
+
+const stripe = Stripe(process.env.STRIPE_SECRET)
+
 export const createConnectAccount = async (req, res) => {
-  console.log('REQ USER FROM REQUIRE_SIGNIN MIDDLEWARE', req.user)
-  console.log('YOU HIT CREATE ACCOUNT ENDPOINT')
+  // 1. find user from db
+  const user = await User.findById(req.user._id).exec()
+  console.log('USER ==> ', user)
+  // 2. if user don't have stripe_account_id yet, create now
+
+  if (!user.stripe_account_id) {
+    const account = await stripe.accounts.create({
+      type: 'express',
+    })
+    console.log('ACCOUNT ===> ', account)
+    user.stripe_account_id = account.id
+    user.save()
+  }
 }
